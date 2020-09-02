920db7e31f74917cbbdd888de537aa6464748465
jimingoal <jimingoal@gmail.com>
2020-09-02 23:33:18 +0900

    제목, 소제목 추가

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -4,19 +4,42 @@ class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlue,
-      body: SafeArea(
-        child: Column(
-          children: [
-            CircleAvatar(
-              child: Icon(Icons.list),
-              backgroundColor: Colors.white,
-              radius: 30,
-              foregroundColor: Colors.lightBlueAccent,
+        backgroundColor: Colors.lightBlue,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  child: Icon(Icons.list),
+                  backgroundColor: Colors.white,
+                  radius: 30,
+                  foregroundColor: Colors.lightBlueAccent,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 30,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      )
-    );
+          ),
+        ));
   }
 }
