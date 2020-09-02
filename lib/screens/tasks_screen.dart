36cedeb93b5a7b4c6dbd86f3951d7710f300d336
jimingoal <jimingoal@gmail.com>
2020-09-02 23:23:00 +0900

    아이콘추가

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -3,6 +3,20 @@ import 'package:flutter/material.dart';
 class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: SafeArea(
+        child: Column(
+          children: [
+            CircleAvatar(
+              child: Icon(Icons.list),
+              backgroundColor: Colors.white,
+              radius: 30,
+              foregroundColor: Colors.lightBlueAccent,
+            ),
+          ],
+        ),
+      )
+    );
   }
 }
